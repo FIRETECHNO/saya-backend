19a4cea204a7b41d858db9e47a3a7eaac5bfb00b
Jet-Green <grishadzyin@gmail.com>
2025-11-28 12:53:26 +0500

    add admin page

--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -1,6 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { HydratedDocument } from 'mongoose';
-import { CompanyFromDadataClass, CompanyFromDadataSchema } from './company.schema';
 
 export type UserDocument = HydratedDocument<UserClass>;
 
@@ -27,46 +26,11 @@ export class UserClass {
   password: string;
 
   @Prop({
-    type: Number,
-    required: false,
-    default: null
-  })
-  tgId: number
-
-  @Prop({
-    type: String,
-    required: false,
-    default: null
-  })
-  tgUsername: string
-
-  @Prop({
     type: Array,
     default: [],
     required: false,
   })
   roles: string[];
-
-  @Prop({
-    type: Array,
-    default: [],
-    required: false
-  })
-  avatars: string[];
-
-  @Prop({
-    type: CompanyFromDadataSchema,
-    required: false,
-    default: null,
-  })
-  company: CompanyFromDadataClass | null;
-
-  @Prop({
-    type: Boolean,
-    required: false,
-    default: false
-  })
-  isModerated: boolean
 }
 
 export const UserSchema = SchemaFactory.createForClass(UserClass);
